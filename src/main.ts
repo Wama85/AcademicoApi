536ca2d3f86fb77d6a3a2420d04c55fed7c2dfce
Wama85 <wi_an9@hotmail.com>
2024-11-29 21:33:04 -0400

    cambio de localhost a todo

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:4200',  // Origen permitido (tu app Angular)
+    origin: '*',  // Origen permitido (tu app Angular)
     methods: 'GET,POST,PUT,DELETE,PATCH',   // Métodos permitidos
     credentials: true                 // Si necesitas enviar cookies o cabeceras autorizadas
   });
